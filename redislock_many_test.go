302b656a5fe600a7694f9ba5072dbeac1677b4b9
FJSDS <Fengjing520!>
2023-06-06 16:35:53 +0800

    fix test

--- redislock_many_test.go
+++ redislock_many_test.go
@@ -3,6 +3,7 @@ package redislock
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/require"
@@ -14,31 +15,27 @@ var redisClient = redis.NewClient(&redis.Options{
 	Password: "",
 })
 
-
-
 func TestClient_ObtainMany(t *testing.T) {
-	r:=require.New(t)
-	c:=New(redisClient)
+	r := require.New(t)
+	c := New(redisClient)
 
-
-
-	keys:=[]string{"123","234"}
-	lock,err:=c.ObtainMany(context.Background(),10,&Options{
-		RetryStrategy: ExponentialBackoff(10,512),
-	},keys...)
+	keys := []string{"123", "234"}
+	lock, err := c.ObtainMany(context.Background(), 10, &Options{
+		RetryStrategy: ExponentialBackoff(10*time.Millisecond, 512*time.Millisecond),
+	}, keys...)
 	r.NoError(err)
 	defer lock.Release(context.Background())
-	_,err = c.ObtainMany(context.Background(),1,&Options{
-		RetryStrategy: ExponentialBackoff(10,512),
-	},keys...)
+	_, err = c.ObtainMany(context.Background(), 1, &Options{
+		RetryStrategy: ExponentialBackoff(10*time.Millisecond, 512*time.Millisecond),
+	}, keys...)
 	r.Error(err)
-	_,err = c.ObtainMany(context.Background(),1,&Options{
-		RetryStrategy: ExponentialBackoff(10,512),
-	},keys[0],"2321")
+	_, err = c.ObtainMany(context.Background(), 1, &Options{
+		RetryStrategy: ExponentialBackoff(10*time.Millisecond, 512*time.Millisecond),
+	}, keys[0], "2321")
 	r.Error(err)
-	_,err = c.ObtainMany(context.Background(),1,&Options{
-		RetryStrategy: ExponentialBackoff(10,512),
-	},"31244","2321")
+	_, err = c.ObtainMany(context.Background(), 1, &Options{
+		RetryStrategy: ExponentialBackoff(10*time.Millisecond, 512*time.Millisecond),
+	}, "31244", "2321")
 	r.NoError(err)
 
 }
